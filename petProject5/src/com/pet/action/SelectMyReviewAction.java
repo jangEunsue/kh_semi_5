b2ce33b2f5fbd24a3a0241a483607ef0000b4ffe
jangEunsue <63176752+jangEunsue@users.noreply.github.com>
2022-05-13 14:46:01 +0900

    Update SelectMyReviewAction.java
    ReviewDAO.selectMyReview    SalesDAO.selectSaleProductcont 매개변수 수정

--- petProject5/src/com/pet/action/SelectMyReviewAction.java
+++ petProject5/src/com/pet/action/SelectMyReviewAction.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
 
 import com.pet.controller.Action;
 import com.pet.controller.ActionForward;
@@ -19,21 +19,20 @@ public class SelectMyReviewAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		// 아이디와 상품번호를 이용해 주문한 상품의 리뷰를 확인하는 비지니스 로직
+		// 주문번호를 이용해 상품의 리뷰를 확인하는 비지니스 로직
 		
-		HttpSession session = request.getSession();
-		String userId = (String)session.getAttribute("sessionID");
+		
 		int no = Integer.parseInt(request.getParameter("no").trim());
-		int pnum = Integer.parseInt(request.getParameter("pno").trim());
+		
 		
 		ReviewDAO dao = ReviewDAO.getInstance();
 		
-		ReviewDTO dto = dao.selectMyReview(userId, no, pnum);
+		ReviewDTO dto = dao.selectMyReview(no);
 		
 		ActionForward forward = new ActionForward();
 		
 		SalesDAO sDao = SalesDAO.getInstance();
-		SalesDTO sDto = sDao.selectSaleProductcont(pnum ,no, userId);
+		SalesDTO sDto = sDao.selectSaleProductcont(no);
 		
 		request.setAttribute("sales", sDto);
 		
