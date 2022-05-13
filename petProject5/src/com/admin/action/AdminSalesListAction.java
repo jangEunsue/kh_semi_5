930ded61c31a88d83ec5b486f078759938cb73d5
yupka <90681175+yupka@users.noreply.github.com>
2022-05-13 19:55:15 +0900

    페이징 처리, 검색 기능 update

--- petProject5/src/com/admin/action/AdminSalesListAction.java
+++ petProject5/src/com/admin/action/AdminSalesListAction.java
@@ -18,13 +18,62 @@ public class AdminSalesListAction implements Action {
 		// 결제 테이블 DB에 저장된 전체 목록을 조회하여
 		// 매출 관리 페이지(view page)로 이동시키는 비지니스 로직.
 		
+		// 페이징 처리 작업 진행
+		int rowsize = 10;		// 한 페이지 당 보여질 게시물의 수
+		int block = 5;			// 아래에 보여질 페이지의 최대 수 - 예)[1][2][3] / [3][4][5]
+		int totalRecord = 0;	// DB상의 게시물의 전체 수
+		int allPage = 0;		// 전체 페이지 수
+		
+		int page = 0;			// 현재 페이지 변수
+		
+		if(request.getParameter("page") != null) {
+			page = Integer.parseInt(request.getParameter("page").trim());
+		} else {	// 처음으로 [전체 게시물 목록] a 태그를 선택한 경우
+			page = 1;
+		}
+		
+		// 해당 페이지에서 시작 번호
+		int startNo = (page * rowsize) - (rowsize - 1);
+		
+		// 해당 페이지에서 끝 번호
+		int endNo = (page * rowsize);
+		
+		// 해당 페이지에서 시작 블럭
+		int startBlock = (((page - 1) / block) * block) + 1;
+		
+		// 해당 페이지에서 끝 블럭
+		int endBlock = (((page - 1) / block) * block) + block;
+		
 		SalesDAO dao = SalesDAO.getInstance();
 		
-		List<SalesDTO> list = dao.getSalesList();
+		// DB상의 전체 게시물 수를 확인하는 메서드 호출
+		totalRecord = dao.getProductCount();
+		
+		// 전체 게시물의 수를 한 페이지당 보여질 게시물의 수로 나누어 주어야 함.
+		// 이 과정을 거치면 전체 페이지 수가 나오게 됨.
+		// 전체 페이지 수가 나올 때 나머지가 있으면 무조건 페이지 수를 하나 올려주어야 함.
+		allPage = (int)Math.ceil(totalRecord / (double)rowsize);
+		
+		if(endBlock > allPage) {
+			endBlock = allPage;
+		}
+		
+		List<SalesDTO> list = dao.getSalesList(page, rowsize);
 		
 		int psum = dao.getSalesPsum();
 		int transcostsum = dao.getSalesTranscostsum();
 		int sumall = dao.getSalesSumall();
+		
+		// 지금까지 페이징 처리 시 작업했던 모든 값들을 view page로 이동시키자.
+		request.setAttribute("page", page);
+		request.setAttribute("rowsize", rowsize);
+		request.setAttribute("block", block);
+		request.setAttribute("totalRecord", totalRecord);
+		request.setAttribute("allPage", allPage);
+		request.setAttribute("startNo", startNo);
+		request.setAttribute("endNo", endNo);
+		request.setAttribute("startBlock", startBlock);
+		request.setAttribute("endBlock", endBlock);
 		
 		request.setAttribute("salesList", list);
 		request.setAttribute("psum", psum);
