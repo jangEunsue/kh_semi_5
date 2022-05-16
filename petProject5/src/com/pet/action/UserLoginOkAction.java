36bcfe980ce2bf789afb18979a1d501b6efdff90
jangEunsue <63176752+jangEunsue@users.noreply.github.com>
2022-05-16 14:44:40 +0900

    유나님 git 수정

--- petProject5/src/com/pet/action/UserLoginOkAction.java
+++ petProject5/src/com/pet/action/UserLoginOkAction.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpSession;
 
 import com.pet.controller.Action;
 import com.pet.controller.ActionForward;
+import com.pet.model.AdminDAO;
+import com.pet.model.AdminDTO;
+import com.pet.model.OrderDAO;
 import com.pet.model.PetMemberDAO;
 import com.pet.model.PetMemberDTO;
 
@@ -20,47 +23,97 @@ public class UserLoginOkAction implements Action {
 		String user_id = request.getParameter("user_id").trim();
 		String user_pwd = request.getParameter("user_pwd").trim();
 		
-		PetMemberDAO dao = PetMemberDAO.getInstance();
-		int check = dao.userPwdCheck(user_id, user_pwd);
-		
 		ActionForward forward = new ActionForward();
+		if (user_id.equals("admin")) {
+			String m_id = user_id;
+			
+			String m_pwd = user_pwd;
+			
+			AdminDAO dao = AdminDAO.getInstance();
+					
+			int check = dao.adminCheck(m_id, m_pwd);
+			
+			HttpSession session = request.getSession();
+			
+			forward = new ActionForward();
+			
+			PrintWriter out = response.getWriter();
+			
+			OrderDAO o_dao = OrderDAO.getInstance();
+			
+			int del_b = o_dao.CountDel_b();
+			int del_i = o_dao.CountDel_i();
+			int del_a = o_dao.CountDel_a();
+			int pay_b = o_dao.CountPay_b();
+			int pay_a = o_dao.CountPay_a();
+			
+			if(check > 0) {
+				// 관리자 정보
+				AdminDTO dto = dao.getAdmin(m_id);
+				
+				session.setAttribute("adminId", dto.getM_id());
+				session.setAttribute("adminName", dto.getM_name());
+				session.setAttribute("del_b", del_b);
+				session.setAttribute("del_i", del_i);
+				session.setAttribute("del_a", del_a);
+				session.setAttribute("pay_b", pay_b);
+				session.setAttribute("pay_a", pay_a);
 
-		PrintWriter out = response.getWriter();
-		HttpSession session = request.getSession();
-		
-		
-		if(user_id.equals("admin")) {	// 임시 관리자 로그인
-			session.setAttribute("sessionID", "admin");
+				forward.setRedirect(false);
+				forward.setPath("admin1/admin_main.jsp");
+				
+			}else if(check == -1) {
+				// 비번이 틀린 경우
+				out.println("<script>");
+				out.println("alert('관리자 비밀번호가 틀립니다.')");
+				out.println("history.back()");
+				out.println("</script>");
+				
+				
+			}else {
+				// 존재하지 않는 관리자
+				out.println("<script>");
+				out.println("alert('존재하지 않은 관리자입니다.')");
+				out.println("history.back()");
+				out.println("</script>");
+			}
 			
-			forward.setRedirect(false);
-			forward.setPath("./admin1/admin_main.jsp"); // 관리자 메인 페이지로 이동
-		}
-		else if (check > 0) {
-			// 회원인 경우
-			PetMemberDTO dto = dao.getMember(user_id);
-			session.setAttribute("userId", dto.getM_id());
-			session.setAttribute("userName", dto.getM_name());
-			session.setAttribute("sessionID", dto.getM_id());
+		}else { // 일반회원
+			PetMemberDAO dao = PetMemberDAO.getInstance();
+			int check = dao.userPwdCheck(user_id, user_pwd);
 			
-			forward.setRedirect(true);
-			forward.setPath("pet_main.do"); // 메인 페이지로 이동
+
+			PrintWriter out = response.getWriter();
+			HttpSession session = request.getSession();
 			
-		}else if (check == -1) {
-			// 비밀번호가 틀린 경우
-			out.println("<script>");
-			out.println("alert('비밀번호를 확인해주세요')");
-			out.println("history.back()");
-			out.println("</script>");
-		}else if(check == 0){
-			// 회원이 아닌 경우(아이디가 없거나 아이디가 틀린 경우)
-			out.println("<script>");
-			out.println("alert('존재하지 않는 아이디 입니다.')");
-			out.println("history.back()");
-			out.println("</script>");
+			if (check > 0) {
+				// 회원인 경우
+				
+				PetMemberDTO dto = dao.getMember(user_id);
+				session.setAttribute("userId", dto.getM_id());
+				session.setAttribute("userName", dto.getM_name());
+				session.setAttribute("sessionID", dto.getM_id());
+				
+				forward.setRedirect(true);
+				forward.setPath("pet_main.do"); // 메인 페이지로 이동
+				
+			}else if (check == -1) {
+				// 비밀번호가 틀린 경우
+				out.println("<script>");
+				out.println("alert('비밀번호를 확인해주세요')");
+				out.println("history.back()");
+				out.println("</script>");
+			}else if(check == 0){
+				// 회원이 아닌 경우(아이디가 없거나 아이디가 틀린 경우
+				out.println("<script>");
+				out.println("alert('존재하지 않는 아이디 입니다.')");
+				out.println("history.back()");
+				out.println("</script>");
+			}
+			
 		}
 		
 		return forward;
-		
 	}
 
 }
